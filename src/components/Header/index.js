5210293b26ebed6fe08fb53d8ea3134b9dc09851
Augusto Nunes Zacarias <augustonz2010@hotmail.com>
2021-05-14 01:39:32 -0300

    Logout only in tasks screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { useApiContext } from '../../contexts/apiContext';
 import {Container, Logo, LogoutIcon} from './styles.js';
 
-export default function Header(props) {
+export default function Header({scene,navigation}) {
 
     const { logout } = useApiContext();
 
     async function handleSubmitLogout() {
         const response = await logout();
-        if (response.error){
-            setError(response.error);
-        } else {
-            navigation.goBack();
-        }
+        if (!response.error){
+            navigation.navigate('Login');
+        } 
     }
 
     return (
         <Container>
             <Logo source={require('../../assets/logo.png')} />
-            <LogoutIcon name='sign-out' onPress={handleSubmitLogout}/>
+            {scene.route.name=='Tasks'?
+            <LogoutIcon name='sign-out' onPress={handleSubmitLogout}/>:null}
         </Container>
     )
 }
